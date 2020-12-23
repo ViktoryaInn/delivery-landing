66110a027f1ba7d9b7733fad7c8c1fa5fd5beade
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-12-23 15:11:48 +0300

    add slider, but it's don't work perfectly...

--- src/js/header.js
+++ src/js/header.js
@@ -17,7 +17,137 @@ window.addEventListener('click', (e) => {
         modalWindow.classList.toggle("modal__window--active");
     }
 });
+/*
+let slides = document.querySelectorAll('div.slider__item');
+console.log(slides.length);
 
-// buttonLogin.onClick = function(){
-//     console.log("something");
-// };
+let currentSlide = 0;
+let timerID;
+
+window.onload = function(){
+    showSlide(currentSlide);
+    timerID = setInterval(() => showNextSlide(), 5000);
+}
+
+function showNextSlide(){
+    console.log("next");
+    if(currentSlide == slides.length - 1){
+        showSlide(0);
+    }else{
+        showSlide(currentSlide + 1);
+    }
+}
+
+function showSlide(index){
+    currentSlide = index;
+    for(let i = 0; i < slides.length; i++){
+        if(i != currentSlide){
+            slides[i].style = "display: none;"
+        }else{
+            slides[i].style = "display: flex;"
+        }
+    }
+}*/
+
+let slider = document.querySelector('.slider');
+let sliderContainer = document.querySelector('.slider__items');
+let sliderItems = document.querySelectorAll('div.slider__item');
+
+console.log(sliderItems.length);
+
+let indicatorIndex = 0;
+let indicatorIndexMax = sliderItems.length - 1;
+let currentPosition = 0;
+let transformValue = 0;
+let transformStep = 100;
+let indicatorItems;
+let timerId;
+
+let itemsArray = [];
+for(let i = 0; i < sliderItems.length; i++){
+  itemsArray.push({item: sliderItems[i], position: i, transform: 0});
+}
+
+console.log(itemsArray);
+
+let position = {
+  getItemIndex: function(mode) {
+    console.log("get item index");
+    let index = 0;
+    for(let i = 0; i < itemsArray.length; i++){
+      if((itemsArray[i].position < itemsArray[index].position && mode === 'min') || (itemsArray[i].position > itemsArray[index].position && mode === 'max')){
+        index = i;
+      }
+    }
+    return index;
+  },
+  getItemPosition: function(mode){
+    console.log("get item position");
+    return itemsArray[position.getItemIndex(mode)].position;
+  }
+};
+
+function moveSlide(direction){
+    console.log("move slide");
+    let nextItem;
+    let currentIndicator = indicatorIndex;
+    if(direction === 'next'){
+      currentPosition++;
+      if(currentPosition > position.getItemPosition('max')){
+        nextItem = position.getItemIndex('min');
+        itemsArray[nextItem].position = position.getItemPosition('max') + 1;
+        itemsArray[nextItem].transform += itemsArray.length * 100;
+        itemsArray[nextItem].item.style.transform = `translateX(${itemsArray[nextItem].transform}%)`;
+      }
+  
+      transformValue -= transformStep;
+      indicatorIndex = indicatorIndex + 1; //
+      if(indicatorIndex > indicatorIndexMax){
+        indicatorIndex = 0;
+      }
+    }else{
+      currentPosition--;
+      if(currentPosition < position.getItemPosition('min')){
+        nextItem = position.getItemIndex('max');
+        itemsArray[nextItem].position = position.getItemPosition('min') - 1;
+        itemsArray[nextItem].transform -= itemsArray.length * 100;
+        itemsArray[nextItem].item.style.transform = `translateX(${itemsArray[nextItem].transform}%)`;
+      }
+  
+      transformValue += transformStep;
+      indicatorIndex--; //
+      if(indicatorIndex < 0) {
+        indicatorIndex = indicatorIndexMax;
+      }
+    }
+  
+    sliderContainer.style.transform = `translateX(${transformValue}%)`;
+    //indicatorItems[currentIndicator].classList.remove('active');
+    //indicatorItems[indicatorIndex].classList.add('active');
+  }
+
+  function moveTo(index){
+    let i = 0;
+    let direction = (index > indicatorIndex) ? 'next' : 'prev';
+    while(index != indicatorIndex && i <= indicatorIndexMax){
+      moveSlide(direction);
+      i++;
+    }
+  }
+  
+  let defaultDirection = 'next';
+  let delayAutoPlay = 5000;
+  
+  function startAutoPlay(){
+    stopAutoPlay();
+    console.log("start auto play");
+    timerId = setInterval(() => moveSlide(defaultDirection), delayAutoPlay);
+  }
+  
+  function stopAutoPlay(){
+    clearInterval(timerId);
+  }
+
+  window.onload = function(){
+    startAutoPlay();
+  }
